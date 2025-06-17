e99385aa7a2f51c2cc4055ab86fc8dde25ed2ddc
Perg01 <jsatshs@gmail.com>
2025-06-16 18:19:08 -0700

    Update the landing page for users that aren't logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,45 @@
-export default function Home() {
+import { Button } from "../components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+import Link from "next/link";
+
+export default function LandingPage() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      {/* Hero Section */}
+      <div className="max-w-3xl text-center space-y-4">
+        <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl md:text-6xl lg:text-7xl">
+          The Perfect Vault for Your AI Conversations
+        </h1>
+        <p>
+          Stop losing brilliant ideas and important answers in endless chat
+          histories. With PromptVault, you can save, organize with folders and
+          tags, and instantly find your most valuable AI interactions.
+        </p>
+      </div>
+
+      {/* Call-to-Action Buttons */}
+      <div className="mt-8">
+        {/* This content is only shown to users who are not logged in. */}
+        <SignedOut>
+          <div className="flex flex-col sm:flex-row gap-4 justify-center">
+            <SignInButton mode="modal">
+              <Button size="lg">Get Started - It&apos;s Free</Button>
+            </SignInButton>
+            <SignUpButton mode="modal">
+              <Button asChild size="lg" variant="secondary">
+                Log in
+              </Button>
+            </SignUpButton>
+          </div>
+        </SignedOut>
+
+        {/* This content is only shown to users that are signed in. */}
+        <SignedIn>
+          <Button asChild size="lg">
+            <Link href={"/dashboard"}>Go to Vault</Link>
+          </Button>
+        </SignedIn>
+      </div>
     </div>
   );
 }
