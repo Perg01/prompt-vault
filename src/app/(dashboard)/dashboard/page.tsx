e5dc349657d915cb11bc0ea320f033ab9d0ee66d
Perg01 <jsatshs@gmail.com>
2025-07-09 12:25:36 -0700

    Delete auth routes since no longer needed Update env variables to change sign-in/sign-up routes for unauthenticated users

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -18,7 +18,7 @@ export default async function DashboardPage() {
   const { userId } = await auth();
 
   if (!userId) {
-    redirect("/");
+    redirect("/sign-in");
   }
 
   // Fetches folder for the current user
