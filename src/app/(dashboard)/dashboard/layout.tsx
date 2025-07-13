c3fedaaaf902e3873fd27b9d04ba1bff9c2ce345
Perg01 <jsatshs@gmail.com>
2025-07-12 21:54:40 -0700

    Add back the layout file for dashboard

---
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+// src/app/(dashboard)/dashboard/layout.tsx
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { userId } = await auth();
+
+  // If there is no userId, redirect to the dedicated sign-in page.
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  // If the user is logged in, show the page.
+  return <>{children}</>;
+}
