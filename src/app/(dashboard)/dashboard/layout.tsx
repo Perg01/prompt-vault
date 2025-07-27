ced5ae9aef1959be3c79312fa89c63e212f352d9
Perg01 <jsatshs@gmail.com>
2025-07-26 22:18:05 -0700

    Update dashboard layout

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -10,7 +10,7 @@ export default async function DashboardLayout({
   const { userId } = await auth();
 
   if (!userId) {
-    redirect("/");
+    redirect("/sign-in");
   }
 
   return <>{children}</>;
